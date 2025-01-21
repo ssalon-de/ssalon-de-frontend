e8e92f45922d19c1cdcdf13edb0d346e44f4e881
강동희 <dhkang@innogrid.com>
2025-01-21 17:14:37 +0900

    chore: service-header 래퍼 변경

--- src/app/(afterAuth)/service-types/components/service-types-header.tsx
+++ src/app/(afterAuth)/service-types/components/service-types-header.tsx
@@ -2,7 +2,7 @@
 
 import { NewServiceTypeDialog } from "@/app/(afterAuth)/service-types/components/create-service-types-dialog";
 import { Button } from "@/components/ui/button";
-import { CardTitle } from "@/components/ui/card";
+import { CardHeader, CardTitle } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -16,17 +16,19 @@ export default function ServicTypesHeader() {
   };
 
   return (
-    <div className="flex justify-between items-center">
-      <CardTitle className="text-2xl font-bold">서비스 관리</CardTitle>
-      <Button onClick={() => setOpenDialog(true)}>
-        <Plus className="mr-2 h-4 w-4" />
-        서비스 생성
-      </Button>
-      <NewServiceTypeDialog
-        open={openDialog}
-        onOpenChange={setOpenDialog}
-        afterCreateServiceType={afterMutateServiceType}
-      />
-    </div>
+    <CardHeader>
+      <div className="flex justify-between items-center">
+        <CardTitle className="text-2xl font-bold">서비스 관리</CardTitle>
+        <Button onClick={() => setOpenDialog(true)}>
+          <Plus className="mr-2 h-4 w-4" />
+          서비스 생성
+        </Button>
+        <NewServiceTypeDialog
+          open={openDialog}
+          onOpenChange={setOpenDialog}
+          afterCreateServiceType={afterMutateServiceType}
+        />
+      </div>
+    </CardHeader>
   );
 }
--- src/app/(afterAuth)/service-types/page.tsx
+++ src/app/(afterAuth)/service-types/page.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -12,9 +12,7 @@ import ServicTypesHeader from "./components/service-types-header";
 export default function Page() {
   return (
     <Card>
-      <CardHeader>
-        <ServicTypesHeader />
-      </CardHeader>
+      <ServicTypesHeader />
       <CardContent>
         <Table>
           <TableHeader>
