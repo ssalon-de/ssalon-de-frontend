110b060b798fdc0ec236f231ccdede93bdc021f5
kangactor123 <kangactor123@naver.com>
2025-02-01 14:39:30 +0900

    feat: 모바일 로고 클릭 시 대시보드 라우팅

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -2,13 +2,15 @@
 
 import useUserStore from "@/zustand/user";
 import { Scissors } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export function Logo() {
+  const router = useRouter();
   const { user } = useUserStore();
   return (
-    <div className="flex items-center">
+    <div className="flex items-center" onClick={() => router.push("/")}>
       <Scissors className="w-8 h-8 text-blue-600" />
-      <span className="text-xl font-bold text-gray-800 ml-2">
+      <span className="ml-2 text-xl font-bold text-gray-800">
         {user.company}
       </span>
     </div>
