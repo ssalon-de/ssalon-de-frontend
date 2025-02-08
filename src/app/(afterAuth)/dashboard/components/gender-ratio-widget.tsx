df7be5453be2fbee8972a367f5eb66e0e2371b24
kangactor123 <kangactor123@naver.com>
2025-02-08 12:10:41 +0900

    feat: 성별 비율 위젯 색상 변경, 툴팁 라벨 추가

--- src/app/(afterAuth)/dashboard/components/gender-ratio-widget.tsx
+++ src/app/(afterAuth)/dashboard/components/gender-ratio-widget.tsx
@@ -5,8 +5,43 @@ import { GenderRatio } from "@/queries/dashboard/type";
 import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
 import { useMemo } from "react";
-import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  ResponsiveContainer,
+  Legend,
+  Tooltip,
+  PieLabel,
+} from "recharts";
 import EmptyWidget from "./empty-widget";
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel: PieLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+      fontSize={13}
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
 export function GenderRatioWidget() {
   const router = useRouter();
@@ -23,7 +58,7 @@ export function GenderRatioWidget() {
     [data]
   );
 
-  const COLORS = ["#0088FE", "#FF8042"];
+  const COLORS = ["#8884d8", "#82ca9d"];
 
   const isEmpty = Object.values(data ?? {}).every((value) => !value);
 
@@ -40,14 +75,20 @@ export function GenderRatioWidget() {
           cy="50%"
           labelLine={false}
           outerRadius={80}
+          label={renderCustomizedLabel}
           fill="#8884d8"
           dataKey="value"
         >
           {chartData.map((_, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            <Cell
+              key={`cell-${index}`}
+              style={{ outline: "none" }}
+              fill={COLORS[index % COLORS.length]}
+            />
           ))}
         </Pie>
         <Legend />
+        <Tooltip />
       </PieChart>
     </ResponsiveContainer>
   );
