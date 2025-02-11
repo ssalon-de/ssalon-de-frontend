aac5d6c90ac8619b35ba0d884fa76eacd74b28a9
kangactor123 <kangactor123@naver.com>
2025-02-11 21:38:58 +0900

    feat: 로고 APP NAME으로 변경

--- src/shared/ui/logo.tsx
+++ src/shared/ui/logo.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import useUserStore from "@/zustand/user";
 import { Scissors } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { APP_NAME } from "../constants/app";
 
 export function Logo() {
   const router = useRouter();
-  const { user } = useUserStore();
   return (
     <div className="flex items-center" onClick={() => router.push("/")}>
       <Scissors className="w-8 h-8 text-blue-600" />
-      <span className="ml-2 text-xl font-bold text-gray-800">
-        {user.company}
-      </span>
+      <span className="ml-2 text-xl font-bold text-gray-800">{APP_NAME}</span>
     </div>
   );
 }
