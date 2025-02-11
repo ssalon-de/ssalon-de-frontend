06a163b7a35fc1c0727a03b6ec54a2a61190ef8f
강동희 <kangactor123@naver.com>
2025-02-11 13:54:50 +0900

    refactor: 성별 비율 차트 Label > active shape로 변경

--- src/app/(afterAuth)/dashboard/components/gender-ratio-widget.tsx
+++ src/app/(afterAuth)/dashboard/components/gender-ratio-widget.tsx
@@ -4,48 +4,105 @@ import { useGenderRatio } from "@/queries/dashboard";
 import { GenderRatio } from "@/queries/dashboard/type";
 import dayjs from "dayjs";
 import { useRouter } from "next/navigation";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import {
   PieChart,
   Pie,
   Cell,
   ResponsiveContainer,
   Legend,
-  Tooltip,
-  PieLabel,
+  Sector,
 } from "recharts";
 import EmptyWidget from "./empty-widget";
+import { ActiveShape } from "recharts/types/util/types";
+import { PieSectorDataItem } from "recharts/types/polar/Pie";
 
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel: PieLabel = ({
-  cx,
-  cy,
-  midAngle,
-  innerRadius,
-  outerRadius,
-  percent,
-}) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
+const renderActiveShape: ActiveShape<PieSectorDataItem> = (
+  props: PieSectorDataItem
+) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx = 0,
+    cy = 0,
+    midAngle = 0,
+    innerRadius,
+    outerRadius = 0,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent = 0,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+  const percentage = `${(percent * 100).toFixed(0)}%`;
   return (
-    <text
-      x={x}
-      y={y}
-      fill="white"
-      textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
-      fontSize={13}
-    >
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+        fontSize={10}
+      >
+        {payload.name}
+      </text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fontSize={10}
+        fill="#999"
+      >
+        {percentage}
+      </text>
+    </g>
   );
 };
 
 export function GenderRatioWidget() {
   const router = useRouter();
+  const [activeIndex, setActiveIndex] = useState(0);
   const { data } = useGenderRatio(dayjs().format("YYYY-MM"));
+
+  const handlePieEnter = (_: unknown, index: number) => {
+    setActiveIndex(index);
+  };
 
   const chartData = useMemo(
     () =>
@@ -70,13 +127,14 @@ export function GenderRatioWidget() {
     <ResponsiveContainer width="100%" height={200}>
       <PieChart>
         <Pie
+          activeIndex={activeIndex}
+          activeShape={renderActiveShape}
+          onMouseEnter={handlePieEnter}
           data={chartData}
           cx="50%"
           cy="50%"
-          labelLine={false}
-          outerRadius={80}
-          label={renderCustomizedLabel}
-          fill="#8884d8"
+          innerRadius={30}
+          outerRadius={40}
           dataKey="value"
         >
           {chartData.map((_, index) => (
@@ -88,7 +146,6 @@ export function GenderRatioWidget() {
           ))}
         </Pie>
         <Legend />
-        <Tooltip />
       </PieChart>
     </ResponsiveContainer>
   );
