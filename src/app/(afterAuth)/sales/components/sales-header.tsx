e94ad687512fb21a984572e28c4e7b5ed35e13a9
kangactor123 <kangactor123@naver.com>
2025-02-11 22:14:06 +0900

    feat: 매출 목록 화면 예약 건수 추가, 모바일 레이아웃 변경

--- src/app/(afterAuth)/sales/components/sales-header.tsx
+++ src/app/(afterAuth)/sales/components/sales-header.tsx
@@ -7,20 +7,18 @@ import Link from "next/link";
 
 const SalesHeader = () => {
   return (
-    <>
-      <div className="flex md:items-center flex-col justify-between md:flex-row gap-2 md:gap-0 items-baseline">
-        <PageTitle title="매출 목록" />
-        <div className="md:w-max md:justify-normal w-full flex gap-4 justify-between">
-          <TotalSales />
-          <Link href="/sales/edit">
-            <Button>
-              <Plus className="mr-2 h-4 w-4" />
-              매출 추가
-            </Button>
-          </Link>
-        </div>
+    <div className="flex md:items-center flex-col justify-between md:flex-row gap-4 md:gap-0 items-baseline relative">
+      <PageTitle title="매출 목록" />
+      <div className="md:w-max md:justify-normal w-full flex gap-4 justify-between flex-wrap">
+        <TotalSales />
+        <Link href="/sales/edit" className="absolute right-0 top-0 md:relative">
+          <Button>
+            <Plus className="mr-2 h-4 w-4" />
+            매출 등록
+          </Button>
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/app/(afterAuth)/sales/components/total-sales.tsx
+++ src/app/(afterAuth)/sales/components/total-sales.tsx
@@ -1,27 +1,49 @@
 "use client";
 
 import Spinner from "@/shared/ui/spinner";
-import { useTotalAmount } from "@/queries/sales";
+import { useSales, useTotalAmount } from "@/queries/sales";
 import useDateStore from "@/zustand/date";
 import dayjs from "dayjs";
+import { useMemo } from "react";
 
 export function TotalSales() {
   const { date } = useDateStore();
+  const { data = [] } = useSales(
+    { date: dayjs(date).format("YYYY-MM-DD") },
+    { enabled: !!date }
+  );
+
   const {
     data: amount = 0,
     isLoading,
     isFetching,
   } = useTotalAmount(dayjs(date).format("YYYY-MM-DDTHH:mm"));
-  const loading = isLoading || isFetching;
+
+  const loading = useMemo(
+    () => isLoading || isFetching,
+    [isLoading, isFetching]
+  );
+  const totalCount = useMemo(() => data.length, [data]);
 
   return (
-    <div className="flex gap-2 items-center">
-      <span className="text-sm font-medium">총 매출</span>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <p className="text-2xl font-bold">{amount.toLocaleString()}원</p>
-      )}
+    <div className="flex flex-col md:flex-row gap-2 md:gap-0">
+      <div className="flex gap-2 items-center tracking-wider">
+        <span className="text-sm font-medium text-gray-600">총 매출</span>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <p className="text-2xl font-bold">{amount.toLocaleString()}원</p>
+        )}
+      </div>
+      <div className="hidden md:block border border-[bg-gray-200] m-2 mx-4" />
+      <div className="flex gap-2 items-center tracking-wider">
+        <span className="text-sm font-medium text-gray-600">예약 건수</span>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <p className="text-2xl font-bold">{totalCount.toLocaleString()}건</p>
+        )}
+      </div>
     </div>
   );
 }
