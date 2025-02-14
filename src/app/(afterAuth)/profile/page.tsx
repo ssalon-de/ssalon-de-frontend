ede418b05c46e11350611c172bf786507dcbef29
강동희 <kangactor123@naver.com>
2025-02-14 11:48:56 +0900

    fix: HydrationBoundary 범위 수정

--- src/app/(afterAuth)/profile/page.tsx
+++ src/app/(afterAuth)/profile/page.tsx
@@ -10,37 +10,37 @@ import PageTitle from "@/shared/ui/page-title";
 import Link from "next/link";
 import Profile from "./components/profile";
 import { getQueryClient } from "@/shared/utils/react-query";
-import { getUserQueryOptions } from "@/queries/auth";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { serverFetch } from "@/shared/utils/serverFetch";
 import { User } from "@/queries/auth/type";
+import { getUserQueryOptions } from "@/queries/auth";
 
 export default async function ProfilePage() {
   const queryClient = getQueryClient();
 
-  void queryClient.prefetchQuery({
+  await queryClient.prefetchQuery({
     ...getUserQueryOptions(),
     queryFn: () => serverFetch<User>("/auth/info"),
   });
 
   return (
-    <div className="container">
-      <PageTitle title="개인정보 확인" className="mb-4" />
-      <Card>
-        <CardHeader>
-          <CardDescription>귀하의 개인정보를 확인하세요.</CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-4">
-          <HydrationBoundary state={dehydrate(queryClient)}>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className="container">
+        <PageTitle title="개인정보 확인" className="mb-4" />
+        <Card>
+          <CardHeader>
+            <CardDescription>귀하의 개인정보를 확인하세요.</CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-4">
             <Profile />
-          </HydrationBoundary>
-        </CardContent>
-        <CardFooter>
-          <Link className="w-full" href="/profile/edit">
-            <Button className="w-full">개인정보 수정</Button>
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
+          </CardContent>
+          <CardFooter>
+            <Link className="w-full" href="/profile/edit">
+              <Button className="w-full">개인정보 수정</Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
+    </HydrationBoundary>
   );
 }
