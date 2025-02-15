591a0d6973b084b02f7da5a772485324e6506aab
kangactor123 <kangactor123@naver.com>
2025-02-15 12:15:59 +0900

    fix: 로그인, 회원가입 overflow-auto 제거

--- src/app/(beforeAuth)/layout.tsx
+++ src/app/(beforeAuth)/layout.tsx
@@ -4,7 +4,7 @@ export default function BeforeAuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="flex items-center justify-center h-screen overflow-auto bg-gray-50">
+    <div className="flex items-center justify-center h-screen bg-gray-50">
       {children}
     </div>
   );
