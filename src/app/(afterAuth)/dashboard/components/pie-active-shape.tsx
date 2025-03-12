52aee6b2ce9c5fc0af9574ec3ec2a0932e64dc6f
강동희 <dhkang@innogrid.com>
2025-03-12 18:23:14 +0900

    refactor: pie chart 공통 컴포넌트로 분리

---
+++ src/app/(afterAuth)/dashboard/components/pie-active-shape.tsx
@@ -0,0 +1,83 @@
+import { Sector } from "recharts";
+import { PieSectorDataItem } from "recharts/types/polar/Pie";
+import { ActiveShape } from "recharts/types/util/types";
+
+const renderActiveShape: ActiveShape<PieSectorDataItem> = (
+  props: PieSectorDataItem
+) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx = 0,
+    cy = 0,
+    midAngle = 0,
+    innerRadius,
+    outerRadius = 0,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent = 0,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 15) * cos;
+  const my = cy + (outerRadius + 15) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 15;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+  const percentage = `${(percent * 100).toFixed(0)}%`;
+  return (
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+        fontSize={10}
+      >
+        {payload.value}건
+      </text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={15}
+        textAnchor={textAnchor}
+        fontSize={10}
+        fill="#999"
+      >
+        {percentage}
+      </text>
+    </g>
+  );
+};
+
+export { renderActiveShape };
