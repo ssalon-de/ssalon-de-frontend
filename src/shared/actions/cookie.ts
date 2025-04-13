2e19e679434d9f62a271ca8ef14831e82a13426e
kangactor123 <kangactor123@naver.com>
2025-04-13 17:40:43 +0900

    feat: 토큰 저장 server action 변경, 토큰 키 값 상수화

--- src/shared/actions/cookie.ts
+++ src/shared/actions/cookie.ts
@@ -1,22 +1,41 @@
 "use server";
 
 import { cookies } from "next/headers";
+import { TOKEN } from "../constants/token";
+import { TokenType } from "../types/token";
 
-export async function setTokenInCookie(token: string) {
+export const getCookie = async (key: string): Promise<string | null> => {
+  "use server";
+  const cookieStore = await cookies();
+  const cookie = cookieStore.get(key);
+
+  if (!cookie) {
+    return null;
+  }
+
+  return cookie.value;
+};
+
+export const setTokenInCookie = async (type: TokenType, token: string) => {
   "use server";
   const cookieStore = await cookies();
 
-  cookieStore.set("accessToken", token, {
+  const maxAge = {
+    [TOKEN.ACCESS_TOKEN]: 60 * 60 * 1000, // 1시간
+    [TOKEN.REFRESH_TOKEN]: 60 * 60 * 24 * 7, // 7일
+  };
+
+  cookieStore.set(type, token, {
+    maxAge: maxAge[type],
     secure: process.env.NODE_ENV === "production",
+    httpOnly: type === TOKEN.REFRESH_TOKEN,
     sameSite: "strict",
-    maxAge: 60 * 60 * 1000, // 1시간
   });
-}
+};
 
 export async function removeTokens() {
   "use server";
   const cookieStore = await cookies();
-
-  cookieStore.delete("accessToken");
-  cookieStore.delete("refreshToken");
+  cookieStore.delete(TOKEN.ACCESS_TOKEN);
+  cookieStore.delete(TOKEN.REFRESH_TOKEN);
 }
