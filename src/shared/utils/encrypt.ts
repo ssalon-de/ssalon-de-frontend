a2230e58409b80c7b945b06ddd7483e2c472a27e
kangactor123 <kangactor123@naver.com>
2025-04-13 17:41:04 +0900

    chore: supabase 제거, 자체 로그인 기능 제거로 인해 js-encrypt 제거

--- src/shared/utils/encrypt.ts
+++
@@ -1,6 +0,0 @@
-import CryptoJS from "crypto-js";
-import { SECRET_KEY } from "../constants/env";
-
-export function encryptPassword(password: string): string {
-  return CryptoJS.AES.encrypt(password, SECRET_KEY).toString();
-}
