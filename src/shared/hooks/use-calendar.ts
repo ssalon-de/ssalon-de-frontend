3c762e647626cd77521f12e8fa5d53e1b4897d87
kangactor123 <kangactor123@naver.com>
2025-04-20 14:05:17 +0900

    refactor: calendar 컴포넌트 교체

---
+++ src/shared/hooks/use-calendar.ts
@@ -0,0 +1,30 @@
+import useDateStore from "@/zustand/date";
+import dayjs from "dayjs";
+import { useMemo } from "react";
+import { SelectSingleEventHandler } from "react-day-picker";
+
+type Result = {
+  selectedDate: Date;
+  onChangeDate: SelectSingleEventHandler;
+  today: Date;
+};
+
+export const useCalendar = (): Result => {
+  const today = new Date();
+  const setDate = useDateStore((state) => state.setDate);
+  const date = useDateStore((state) => state.date);
+
+  const onChangeDate: SelectSingleEventHandler = (day) => {
+    if (day) {
+      setDate(dayjs(day).format("YYYY-MM-DD"));
+    }
+  };
+
+  const selectedDate = useMemo(() => dayjs(date).toDate(), [date]);
+
+  return {
+    selectedDate,
+    onChangeDate,
+    today,
+  };
+};
