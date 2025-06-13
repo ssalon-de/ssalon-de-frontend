75cb6409744cd4b18a09587efd29438d37579954
강동희 <dhkang@innogrid.com>
2025-06-13 09:16:59 +0900

    fix: dashboard stale time 변경, 기본 stale time 10>5초로 변경

--- src/shared/constants/app.ts
+++ src/shared/constants/app.ts
@@ -10,8 +10,9 @@ export const AFTER_LOGIN_REDIRECT_PATH = PATH.DASHBOARD;
 
 /** config */
 export const QUERY_RETRY_COUNT = 3;
-export const QUERY_DEFAULT_STALE_TIME = 1000 * 10; // 10초
+export const QUERY_DEFAULT_STALE_TIME = 1000 * 5; // 5초
 export const FILTER_STALE_TIME = QUERY_DEFAULT_STALE_TIME;
+export const DASHBOARD_STALE_TIME = 0; // 0초
 export const API_TIMEOUT = 1000 * 5; // 5초
 export const ACCESS_TOKEN_EXPIRE = 60 * 60 * 1000; // 1시간
 export const REFRESH_TOKEN_EXPIRE = 7 * 24 * 60 * 60 * 1000; // 7일
